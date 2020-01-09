14e687ad4d26916d51b985393e604250e03c7880
Apostolis Gerodimos <gerodiak@auth.gr>
2020-01-09 19:16:00 +0200

    passport service v3 - State: Functional

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ const app = express();
 const server = require("http").createServer(app);
 const bodyParser = require("body-parser");
 var cors = require("cors");
-const passport = require("passport");
+var passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-// var mysql = require("mysql");
-var sequelize = require("./services/sqlService");
+const sequelize = require("./services/sqlService");
 var morgan = require("morgan");
-// const jwt = require('jsonwebtoken');
-
 const session = require("express-session");
 const sessionStore = new session.MemoryStore();
 
@@ -23,18 +20,16 @@ server.listen(process.env.PORT || 8082, () => {
 
 // Hook up the HTTP logger.
 app.use(morgan("dev"));
-//  Hook up Passport for authentication
 
-// Hook the passport strategy.
-// var validateRouter = require('./services/passportStrategyService');
-// app.use('/validate', validateRouter);
-
+// Serve static files like css, images
 app.use("/public", express.static(__dirname + "/public"));
 app.use(cors());
 
+//Be able to parse the body of requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Create Session to be used by passport
 app.use(
   session({
     secret: "sdfsdfasdfasdfasdfsdaasdasdfasd",
@@ -45,16 +40,14 @@ app.use(
   })
 );
 
+// Init the passport.
 app.use(passport.initialize());
 app.use(passport.session());
+require("./services/passportService")(passport);
 
-// app.use(express.json());
-// app.use(express.urlencoded());
-
-//set render engine
+// Set render engine
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "ejs");
-// var router = app.Router();
 
 app.get("/", (req, res) => {
   res.render("sign-in-room8s.ejs");
@@ -64,46 +57,6 @@ app.get("/home", (req, res) => {
   console.log("Is the user authenticated? => " + req.isAuthenticated());
   res.render("main.ejs");
 });
-
-passport.serializeUser((user, done) => {
-  console.log("serialize routine with user id = " + user.id);
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-  console.log("deserialize routine with key:  ");
-  User.findOne({ where: { id: id } }).then(user => done(null, user));
-});
-
-passport.use(
-  "local",
-  new LocalStrategy(
-    { usernameField: "email", passwordField: "password" },
-    (username, password, done) => {
-      potentialUser = { where: { email: username } };
-      User.findOne(potentialUser)
-        .then(function(user, error) {
-          if (!user) {
-            return done(null, false);
-          } else if (error) {
-            return done(error);
-          } else {
-            user.comparePasswords(password, function(error, isMatch) {
-              console.log(isMatch, error);
-              if (isMatch && !error) {
-                return done(null, user);
-              } else {
-                return done(null, false);
-              }
-            });
-          }
-        })
-        .catch(function(error) {
-          res.status(500).json({ message: "There was an error!" });
-        });
-    }
-  )
-);
 
 app.post(
   "/validate",
--- services/passportService.js
+++ services/passportService.js
@@ -1,40 +1,44 @@
-// var passport = require('passport');
-// const LocalStrategy = require('passport-local').Strategy;
-// var User = require('./../model/User.js');
+const LocalStrategy = require("passport-local").Strategy;
+var User = require("./../model/User.js");
 
-// passport.serializeUser((user, done) => {
+function passportConfigure(passport) {
+  passport.serializeUser((user, done) => {
+    console.log("serialize routine with user id = " + user.id);
+    done(null, user.id);
+  });
+  passport.deserializeUser((id, done) => {
+    console.log("deserialize routine with key:  ");
+    User.findOne({ where: { id: id } }).then(user => done(null, user));
+  });
+  passport.use(
+    "local",
+    new LocalStrategy(
+      { usernameField: "email", passwordField: "password" },
+      (username, password, done) => {
+        potentialUser = { where: { email: username } };
+        User.findOne(potentialUser)
+          .then(function(user, error) {
+            if (!user) {
+              return done(null, false);
+            } else if (error) {
+              return done(error);
+            } else {
+              user.comparePasswords(password, function(error, isMatch) {
+                console.log(isMatch, error);
+                if (isMatch && !error) {
+                  return done(null, user);
+                } else {
+                  return done(null, false);
+                }
+              });
+            }
+          })
+          .catch(function(error) {
+            res.status(500).json({ message: "There was an error!" });
+          });
+      }
+    )
+  );
+}
 
-//     console.log("serialize routine with user id = " + user.id);
-//     done(null, user.id);
-// });
-
-// passport.deserializeUser((id, done) => {
-
-//     console.log('deserialize routine with key:  ');
-//     User.findOne({ where: { 'id': id } }).then(user => done(null, user));
-// });
-
-// passport.use('local', new LocalStrategy({ usernameField: 'email', passwordField: 'password' }, (username, password, done) => {
-//     potentialUser = { where: { email: username } };
-//     User.findOne(potentialUser).then(function (user, error) {
-//         if (!user) {
-//             return done(null, false);
-//         } else if (error) {
-//             return done(error);
-//         }
-//         else {
-//             user.comparePasswords(password, function (error, isMatch) {
-//                 console.log(isMatch, error);
-//                 if (isMatch && !error) {
-//                     return done(null, user);
-//                 } else {
-//                     return done(null, false);
-//                 }
-//             });
-//         }
-//     }).catch(function (error) {
-//         res.status(500).json({ message: 'There was an error!' });
-//     });
-// }));
-
-// module.exports = passport;
+module.exports = passportConfigure;
