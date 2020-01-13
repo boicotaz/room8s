6d4beb41369b65f0069c9fca498c1774c315d0aa
Apostolis Gerodimos <gerodiak@auth.gr>
2020-01-13 15:45:59 +0200

    expenses page v2

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,17 @@
+// class Mybutton extends React.Component {
+
+//     render() {
+
+//         return (<a className="btn btn-primary btn-lg" href="#" role="button">Apostolis</a>);
+
+//     }
 
 
+// }
 
 
+// $(document).ready(function () {
 
+
+//     ReactDOM.render(<Mybutton />, document.getElementById("anotherTest"));
+// });
