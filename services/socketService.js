48940d31d0bd67c7c7cb8640aa3f97fb45351e50
Apostolis Gerodimos <gerodiak@auth.gr>
2020-01-15 15:38:42 +0200

    socketService: user logs => a socket is created , user dcs => socket is destroyed. Added groupmember login status

--- services/socketService.js
+++ services/socketService.js
@@ -14,16 +14,50 @@ function socketInit(server){
             groupService.findUsersInGroup(group.getGroupId()).then((users) => {
               let usersIdInGroup;
               usersIdInGroup = users.map(user => user.id);
-              console.log(usersIdInGroup);
-              for(const [client, userInList] of sequenceNumberByClient) {
-                if (usersIdInGroup.includes(userInList.id)) {
-                  client.emit("user_in_my_group_connected", data.user.firstName);
-                  console.log(data.user.firstName);
+              let clientSockets = [];
+              let userNamesThatAreLoggedIn = []; 
+
+              for(const [client, userInLoggedInList] of sequenceNumberByClient) {
+
+                if (usersIdInGroup.includes(userInLoggedInList.id)) {
+                  clientSockets.push(client);
+                  userNamesThatAreLoggedIn.push(userInLoggedInList.firstName);
                 }
               }
+
+              clientSockets.map( clientSocket => clientSocket.emit("user_in_my_group_connected", userNamesThatAreLoggedIn  ));
+              
             });
           });
         });
+
+
+
+        socket.on('disconnect', () => {
+          console.log('user disconnected' , socket.id);
+          let res 
+          for( const [client, userInLoggedInList] of sequenceNumberByClient) {
+            if(socket.id == client.id){
+              console.log('Found dc!!', client.id);
+              groupService.findGroupByUserId(userInLoggedInList.id).then((group) => {
+                // console.log()
+                groupService.findUsersInGroup(group.getGroupId()).then((users) => {
+                  usersIdInGroup = users.map(user => user.id);
+
+                  for(const [clientNested, userInLoggedInListNested] of sequenceNumberByClient) {
+                    
+                    if (usersIdInGroup.includes(userInLoggedInListNested.id)) {
+                      clientNested.emit("user_in_my_group_disconnected" , userInLoggedInList.firstName );
+                    }
+                  } 
+
+                });
+              })
+
+            }
+          }
+
+        })
       });
       return serverSocket;
 } 
